82dd5020ac0a4378e0b9a2d0fc83d4e3ed92def0
Tarabyte <80751447+drunk3nb0at@users.noreply.github.com>
2025-07-12 00:32:20 +0800

    Update index.js

--- themes/index.js
+++ themes/index.js
@@ -462,6 +462,13 @@ export const themes = {
     icon_color: "ffffff",
     bg_color: "35,4158d0,c850c0,ffcc70",
   },
+  dragon_fruit: {
+    title_color: "89DDFF",
+    text_color: "89DDFF",
+    icon_color: "dc589a",
+    border_color: "F2BAD1",
+    bg_color: "212121",
+  },
 };
 
 export default themes;
